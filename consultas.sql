045ae4e12ce700f5d92b8400a87dc1479918fffb
Fabricio Almeida <fabricio.sda@hotmail.com>
2021-05-15 14:46:17 -0300

    Adicionando consultas e casos de uso

--- consultas.sql
+++ consultas.sql
@@ -1,5 +1,27 @@
 -- Arquivo pra adicionar as consultas que iremos criar para a etapa 2 do trabalho
 
+-- [CASOS DE USO]
+-- Listar classificação do campeonato - OK
+-- Listar artilheiros -OK
+-- Listar lideres em assitencias - OK
+-- Listar lideres em cartões amarelos - OK
+-- Listar lideres em cartões amarelos - OK
+-- Listar jogadores com maior número de partidas jogadas - OK
+-- Exibe todas partidas - OK
+-- Exibe todas partidas de um determinado time - TODO
+-- Exibe melhor visitante - TODO
+-- Exibe melhor mandante - TODO
+-- Exibe pior visitante - TODO
+-- Exibe pior mandante - TODO
+-- Consulta comissão tecnica de todos os times - TODO
+-- Consulta comissão tecnica de determinado time - TODO
+-- Exibe publico das partidas - OK
+-- Consulta estatistica das partidas que tiveram jogadores explusos - OK
+-- Exibe jogadores de determinado time no dpt médico - TODO
+-- Exibe número de jogadores de cada time no dpt médico - OK
+-- Exibe os times que tem mais de N jogadores no dpt - OK
+
+-- 10 consultas requeridas pela especificação do trabalho
 -- Retorna nome do time e número de jogadores no DM
 SELECT T.nome, count(D.nome) FROM dadosjogador D INNER JOIN "time" T ON D.id_time = T.id_time NATURAL JOIN dm GROUP BY T.id_time;
 
@@ -8,3 +30,29 @@ SELECT T.nome FROM dadosjogador D INNER JOIN "time" T ON D.id_time = T.id_time N
 
 -- Retorna os dados das partidas que tiveram jogadores expulsos
 SELECT * from partida WHERE NOT EXISTS (SELECT * FROM partida NATURAL JOIN estatisticas WHERE cartoes_vermelhos_mand <> 0 OR cartoes_vermelhos_visit <> 0)
+
+-- Retorna publico das partidas data, nome dos times e ordena por publico crescente
+SELECT ES.apelido, E.publico, T1.nome, T2.nome, P.data_partida FROM estatisticas E INNER JOIN partida P ON P.id_partida = E.id_partida INNER JOIN estadio ES ON ES.id_estadio = P.id_estadio INNER JOIN "time" T1 on T1.id_time = P.time_mandante INNER JOIN "time" T2 ON T2.id_time = p.time_visitante ORDER BY E..publico DESC;
+
+-- Exibe historico de partidas
+SELECT T1.nome, p.gols_mandante, T2.nome, p.gols_visitante, P.data_partida FROM estatisticas E INNER JOIN partida P ON P.id_partida = E.id_partida INNER JOIN "time" T1 on T1.id_time = P.time_mandante INNER JOIN "time" T2 ON T2.id_time = p.time_visitante ORDER BY P.data_partida asc;
+
+
+-- Consultas Extras para funcionalidades basicas
+-- Exibe classificação do campeonato
+SELECT nome, pontuacao, partidas, vitorias, empates, derrotas, cartoes_amarelos, cartoes_vermelhos, gols_pro, gols_contra from "time" ORDER BY pontuacao, vitorias, empates, gols_pro DESC;
+
+-- Exibe classificação de artilheiros
+SELECT D.nome, T.nome, D.gols from dadosjogador D INNER JOIN "time" t ON t.id_time = D.id_time ORDER BY gols DESC;
+
+-- Exibe classificação de lider em assistencias
+SELECT D.nome, T.nome, D.assistencias from dadosjogador D INNER JOIN "time" t ON t.id_time = D.id_time ORDER BY assistencias DESC;
+
+-- Exibe jogadores punidos com artão amarelo
+SELECT D.nome, T.nome, D.cartoes_amarelos from dadosjogador D INNER JOIN "time" t ON t.id_time = D.id_time ORDER BY cartoes_amarelos DESC;
+
+-- Exibe jogadores punidos com artão vermelho
+SELECT D.nome, T.nome, D.cartoes_vermelhos from dadosjogador D INNER JOIN "time" t ON t.id_time = D.id_time ORDER BY cartoes_vermelhos DESC;
+
+-- Exibe jogadores com maior número de partidas jogadas
+SELECT D.nome, T.nome, D.num_partidas from dadosjogador D INNER JOIN "time" t ON t.id_time = D.id_time ORDER BY num_partidas DESC;
